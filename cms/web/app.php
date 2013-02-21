adbaadcd42616c9c00837edafb9f137a1bf68b7b
Daniel Korsak <daniel.korsak@redexperts.net>
2013-02-21 16:01:56 +0100

    Updates

--- cms/web/app.php
+++ cms/web/app.php
@@ -6,11 +6,10 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
 // Use APC for autoloading to improve performance
 // Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-
-$loader = new ApcClassLoader('symfony-sandbox', $loader);
+$prefix = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'symfony-sandbox';
+$loader = new ApcClassLoader($prefix, $loader);
 $loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
