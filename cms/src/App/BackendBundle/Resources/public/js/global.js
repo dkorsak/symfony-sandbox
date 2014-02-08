4be79744a8434dc5cde18a3892737e4525c5833a
Daniel Korsak <daniel.korsak@wp.pl>
2014-02-08 19:34:03 +0100

    Composer, updates

--- cms/src/App/BackendBundle/Resources/public/js/global.js
+++ cms/src/App/BackendBundle/Resources/public/js/global.js
@@ -11,14 +11,14 @@ jQuery(document).ready(function() {
             $icon.removeClass("icon-chevron-down");
             $icon.addClass("icon-chevron-up");
         }
-    }
+    };
 
     // show/hide filters
     jQuery('.show_filters').click(function() {
         jQuery('.filter_container').toggle();
         toggleUpDownIcon(this);
         
-    })
+    });
     // show/hide form fieldset
     jQuery('.form_box_fieldset-collapsed').not(':has(.error)').find('.box-content').hide();
     jQuery('.form_box_fieldset-collapsed').has('.error').find('div.box-icon i').removeClass('icon-chevron-down').addClass('icon-chevron-up');
@@ -31,7 +31,7 @@ jQuery(document).ready(function() {
         if ("on-submit" == jQuery(this).data("on-sumbit")) {
             return false;
         }
-        jQuery(this).data("on-sumbit", "on-submit")
+        jQuery(this).data("on-sumbit", "on-submit");
     });
     jQuery(document).ajaxStart(function() {
         jQuery('#ajax_loader').show();
@@ -40,4 +40,4 @@ jQuery(document).ready(function() {
     jQuery(document).ajaxStop(function() {
         jQuery('#ajax_loader').hide();
     });
-})
+});
